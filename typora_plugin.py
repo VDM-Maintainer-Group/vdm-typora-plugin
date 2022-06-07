589a5f4e422cb474baad35f732f14709481087e7
iamhyc <sudofree@163.com>
2022-06-07 17:16:12 +0800

    update x11 control

--- typora_plugin.py
+++ typora_plugin.py
@@ -1,41 +1,90 @@
 #!/usr/bin/env python3
-import os, psutil
-import time
+import os, re, time, psutil
+import json
+import subprocess as sp
 from pathlib import Path
 from pyvdm.interface import CapabilityLibrary, SRC_API
 
+DBG = 0
+SLOT = 0.40
 PROG_NAME = 'Typora'
+WINDOW_TITLE = '%s.? - Typora'
 
 class TyporaPlugin(SRC_API):
     @staticmethod
-    def _gather_record(raw_result):
-        record = dict()
-        for item in raw_result:
+    def _gather_records(raw_results) -> list:
+        records = list()
+
+        ## collect and filter the raw data
+        _processes = dict()
+        for item in raw_results:
             pid, path = item.split(',', maxsplit=1)
             pid  = int(pid)
             path = path.rstrip('\u0000')
             #
-            if pid not in record.keys():
-                record[pid] = [path]
+            if pid not in _processes.keys():
+                _processes[pid] = [path]
             else:
-                record[pid].append(path)
+                _processes[pid].append(path)
             pass
-        return record
+        # if DBG: print( json.dumps(_processes, indent=4) )
 
-    @staticmethod
-    def _dump_record(fh, record):
-        for pid, items in record.items():
+        ## gather records
+        for pid, items in _processes.items():
             if len(items) == 1:
                 _path = items[0]
+                records.append( (pid,_path) )
+                if DBG: print(f'file [{pid}]: {_path}')
             elif len( psutil.Process(pid).cmdline() ) == 1:
                 _path = ''
             else:
                 _path = os.path.commonpath(items)
-            #
-            if _path: fh.write( _path+'\n' )
+                records.append( (pid,_path) )
+                if DBG: print(f'folder [{pid}]: {_path}')
+            pass
+
+        return records
+
+    def _associate_with_window(self, records) -> dict:
+        results = list()
+
+        windows = self.xm.get_windows_by_name('Typora')
+        windows.sort(key=lambda x:x['xid'])
+        if windows:
+            _pid = windows[0]['pid']
+            records = filter(lambda x:x[0]!=_pid, records)
+
+        for (_pid,_path),w in zip(records, windows):
+            results.append({
+                'name': Path(_path).name,
+                'path': _path,
+                'window': {
+                    'desktop':w['desktop'],
+                    'states':w['states'],
+                    'xyhw':w['xyhw']
+                }
+            })
+
+        return results
+
+    def _rearrange_window(self, records: list):
+        _time = time.time()
+        _limit= len(records) * 2*SLOT
+
+        windows = self.xm.get_windows_by_name('Typora')
+        while len(windows)!=records and time.time()-_time<_limit:
+            time.sleep(SLOT)
+            windows = self.xm.get_windows_by_name('Typora')
+        windows.sort(key=lambda x:x['xid'])
+
+        for stat,w in zip(records, windows):
+            s = stat['window']
+            self.xm.set_window_by_xid( w['xid'], s['desktop'], s['states'], s['xyhw'] )
+
         pass
 
     def onStart(self):
+        self.xm = CapabilityLibrary.CapabilityHandleLocal('x11-manager')
         self.il = CapabilityLibrary.CapabilityHandleLocal('inotify-lookup')
         self.il.register( PROG_NAME )
         return 0
@@ -45,42 +94,50 @@ class TyporaPlugin(SRC_API):
         return 0
 
     def onSave(self, stat_file):
-        # dump raw_result via il
-        raw_result = self.il.dump( PROG_NAME )
-        # gathering record from raw_result
-        record = self._gather_record(raw_result)
-        # write to file
+        ## dump raw_results via il
+        raw_results = self.il.dump( PROG_NAME )
+        ## gathering records from raw_result
+        records = self._gather_records(raw_results)
+        ##
+        records = self._associate_with_window(records)
+        ## write to file
         with open(stat_file, 'w') as f:
-            self._dump_record(f, record)
+            json.dump(records, f)
             pass
         return 0
 
     def onResume(self, stat_file):
+        ## load stat file with failure check
         with open(stat_file, 'r') as f:
-            items = f.readlines()
-            for piece in items:
-                os.system( 'typora %s'%piece )
-            pass
-        time.sleep(1.5)
+            _file = f.read().strip()
+        if len(_file)==0:
+            return 0
+        else:
+            try:
+                records = json.loads(_file)
+            except:
+                return -1
+        ## open windows
+        for item in records:
+            sp.Popen([ 'typora', item['path'] ], start_new_session=True)
+        ## rearrange windows by title
+        self._rearrange_window(records)
         return 0
 
     def onClose(self):
+        ## force close all
         os.system( f'killall {PROG_NAME}' )
         return 0
     pass
 
 if __name__ == '__main__':
-    import sys, json
     _plugin = TyporaPlugin()
     _plugin.onStart()
 
-    # dump raw_result via il
-    raw_result = _plugin.il.dump( PROG_NAME )
-
-    # gathering record from raw_result
-    record = _plugin._gather_record(raw_result)
-    # print( json.dumps(record, indent=4) )
-    
-    # write to file
-    _plugin._dump_record(sys.stdout, record)
+    raw_results = _plugin.il.dump( PROG_NAME )
+    ## gathering records from raw_results
+    records = _plugin._gather_records(raw_results)
+    ## associate with X window
+    records = _plugin._associate_with_window(records)
+    print( json.dumps(records, indent=4) )
     pass
